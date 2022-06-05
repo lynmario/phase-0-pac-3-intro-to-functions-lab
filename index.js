a3afb573e6de1d70faf85c120dac0c8632b2e3b2
maryrine onsare <maryrine.onsare@student.moringaschool.com>
2022-06-05 12:04:37 +0300

    commit tree 690cdfc2111bb0626fe264d9c0349651cf51fa00

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase();
+};
+
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(shout(string));
+}
+
+function logWhisper(string){
+    console.log(whisper(string));
+}
+function sayHiToHeadphonedRoommate(string){
+    if(whisper(string)===string){
+        return "I can't hear you!";
+    }else if(shout(string)===string){
+        return 'YES INDEED!';
+    }else{
+        return "I would love to!";
+    }
+}
